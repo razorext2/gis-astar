1adc64403957ac60e53551c0b2eb8cf85ba20d6f
Local Server <abdi@darkotech.id>
2024-12-11 16:04:40 +0700

    adjust: untuk deploy ke server lokal

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -19,6 +19,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->trustProxies(at: '*');
         // roles
         $middleware->alias([
             'role' => RoleMiddleware::class,
