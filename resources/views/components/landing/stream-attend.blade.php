2c52d8934b3a93506bc23335bdd387784646e69a
Local Server <abdi@darkotech.id>
2024-12-12 09:18:04 +0700

    adjust: fungsi absensi agar bisa digunakan

--- resources/views/components/landing/stream-attend.blade.php
+++ resources/views/components/landing/stream-attend.blade.php
@@ -1,7 +1,7 @@
 <video
   class="dark:ring-gray-700 flex h-60 w-full rounded-lg object-cover p-0 ring-1 ring-black md:h-auto lg:h-[480px] lg:w-[640px]"
   id="video" data-aos="zoom-in" data-aos-delay="100"
-  style="background: url('assets/img/noCamera.webp') center center / cover no-repeat;" autoplay>
+  style="background: url('{{ asset('assets/img/noCamera.webp') }}') center center / cover no-repeat;" autoplay>
 </video>
 <canvas class="absolute left-0 top-0 flex h-60 w-full rounded-lg object-cover p-0 md:h-auto lg:h-[480px] lg:w-[640px]"
   id="overlay">
