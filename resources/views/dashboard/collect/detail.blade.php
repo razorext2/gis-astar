74d8faa9d93cac86f69ab830d3b83879cd56bfcc
razorext2 <razorext2@gmail.com>
2025-01-20 14:38:41 +0700

    update

--- resources/views/dashboard/collect/detail.blade.php
+++ resources/views/dashboard/collect/detail.blade.php
@@ -124,7 +124,7 @@
 							@elseif ($status == 1)
 								<span
 									class="rounded-xl bg-green-100 px-4 py-2 text-sm font-medium text-green-800 ring-1 ring-gray-300 dark:bg-green-900 dark:text-green-300 dark:ring-gray-700">
-									Disetujui. (divalidasi oleh: {{ $user->name }})
+									Disetujui. (divalidasi oleh: {{ $user->name ?? 'N/A' }})
 								</span>
 							@elseif ($status == 2)
 								<span
@@ -134,7 +134,7 @@
 							@else
 								<span
 									class="rounded-xl bg-red-100 px-4 py-2 text-sm font-medium text-red-800 ring-1 ring-gray-300 dark:bg-red-900 dark:text-red-300 dark:ring-gray-700">
-									Laporan di Tolak! (divalidasi oleh: {{ $user->name }})
+									Laporan di Tolak! (divalidasi oleh: {{ $user->name ?? 'N/A' }})
 								</span>
 							@endif
 
