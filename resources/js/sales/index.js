d3cca223dde5ad3fb1c35803ee99a1e679276411
razorext2 <razorext2@gmail.com>
2025-02-17 16:18:03 +0700

    mengembalikan tanggal, gambar on hover: scale = 2

--- resources/js/sales/index.js
+++ resources/js/sales/index.js
@@ -16,86 +16,107 @@ $(document).ready(function () {
 
     const row = response.data.data
     const result = await Swal.fire({
-      title: "Detail",
       html: `
-        <div class="w-full">
-				<div class="grid gap-2 md:grid-cols-2">
-					<div
-						class="col-span-2 flex flex-col items-start justify-center rounded-xl border border-gray-200 bg-gray-50 p-3   lg:col-span-1">
-						<p class="text-sm text-gray-700">Kode Pegawai</p>
-						<p class="text-gray-800 text-base font-medium ">
-							${row.kode_pegawai}
-						</p>
-					</div>
+        <div class=" min-w-96 text-left">
+          
+          <div class="grid gap-1 md:grid-cols-2 text-left">
+            <div
+              class="col-span-2 flex flex-col items-start rounded-xl">
+              <span class="font-semibold">Detail: </span>
+            </div>
 
-					<div
-						class="col-span-2 flex flex-col items-start justify-center rounded-xl border border-gray-200 bg-gray-50 p-3   lg:col-span-1">
-						<p class="text-sm text-gray-700">Nama Pegawai</p>
-						<p class="text-gray-800 text-base font-medium ">
-							${row.pegawai_relasi.full_name}
-						</p>
-					</div>
+            <div
+              class="col-span-2 flex flex-col items-start rounded-xl border border-gray-200 bg-gray-50 p-3 lg:col-span-1">
+              <p class="text-sm text-gray-700">Kode Pegawai</p>
+              <p class="text-gray-800 text-base font-medium ">
+                ${row.kode_pegawai}
+              </p>
+            </div>
 
-					<div
-						class="col-span-2 flex flex-col items-start justify-center rounded-xl border border-gray-200 bg-gray-50 p-3  ">
-						<p class="text-sm text-gray-700">Judul laporan</p>
-						<p class="text-gray-800 text-base font-medium ">
-							${row.title}
-						</p>
-					</div>
+            <div
+              class="col-span-2 flex flex-col items-start rounded-xl border border-gray-200 bg-gray-50 p-3 lg:col-span-1">
+              <p class="text-sm text-gray-700">Nama Pegawai</p>
+              <p class="text-gray-800 text-base font-medium ">
+                ${row.pegawai_relasi.full_name}
+              </p>
+            </div>
 
-					<div
-						class="col-span-2 flex flex-col items-start justify-center rounded-xl border border-gray-200 bg-gray-50 p-3  ">
-						<p class="text-sm text-gray-700">Customer</p>
-						<p class="text-gray-800 text-base font-medium ">
-							${row.customer_name}
-						</p>
-						<a class="text-gray-800 inline-flex text-base font-medium underline "
-							href="https://api.whatsapp.com/send?phone=${row.customer_telp}&text=Halo, %2A${row.title}%2A. %0A%0ASaya %2A${userName}%2A, marketing dari %2APT. Indodacin Presisi Utama%2A. Saya ingin menghubungi Anda terkait pesanan atau layanan yang mungkin Anda butuhkan.%0A%0AJika ada pertanyaan atau ingin berdiskusi lebih lanjut, silakan balas pesan ini.%0A%0ATerima kasih!%F0%9F%98%8A"
-							target="_blank">
-							Chat customer
-						</a>
-					</div>
+          <div
+              class="col-span-2 flex flex-col items-start rounded-xl border border-gray-200 bg-gray-50 p-3 lg:col-span-1"">
+              <p class="text-sm text-gray-700">Waktu Dibuat</p>
+              <p class="text-gray-800 text-base font-medium ">
+                ${new Date(row.created_at).toLocaleString('id-ID')}
+              </p>
+            </div>
 
-					<div
-						class="col-span-2 flex flex-col items-start justify-center rounded-xl border border-gray-200 bg-gray-50 p-3  ">
-						<p class="text-sm text-gray-700">Lokasi checkpoint</p>
-						<span class="text-gray-800 text-base font-medium ">${row.lokasi}</span>
-						<span class="text-xs font-medium text-gray-400 ">
-							<a class="inline-flex underline"
-								href="https://www.google.com/maps/@${row.latitude},${row.longitude},20m/" target="_blank">
-								${row.latitude}, ${row.longitude}
-							</a>
-						</span>
-					</div>
+            <div
+              class="col-span-2 flex flex-col items-start rounded-xl border border-gray-200 bg-gray-50 p-3 lg:col-span-1"">
+              <p class="text-sm text-gray-700">Waktu Diupdate</p>
+              <p class="text-gray-800 text-base font-medium ">
+                ${new Date(row.updated_at).toLocaleString('id-ID')}
+              </p>
+            </div>
 
-					<div
-						class="col-span-2 flex flex-col items-start justify-center rounded-xl border border-gray-200 bg-gray-50 p-3  ">
-						<p class="mb-2 text-sm text-gray-700">Dokumentasi</p>
-						<div class="relative me-2 flex-none items-center gap-4 rounded-xl p-2">
-				  		<img class="h-52 w-52 rounded-xl object-cover transition duration-300 ease-in-out hover:scale-110" id="documentations" src="${row.photo_collect_relasi[0].photourl}" alt="" onclick="javascript:void(0)" loading="lazy">
-						</div>
-					</div>
+            <div
+              class="col-span-2 flex flex-col items-start rounded-xl border border-gray-200 bg-gray-50 p-3">
+              <p class="text-sm text-gray-700">Judul laporan</p>
+              <p class="text-gray-800 text-base font-medium ">
+                ${row.title}
+              </p>
+            </div>
 
-					<div
-						class="col-span-2 flex flex-col items-start justify-center rounded-xl border border-gray-200 bg-gray-50 p-3   lg:col-span-1">
-						<p class="text-sm text-gray-700">Keterangan</p>
-						<p class="text-gray-800 text-base font-medium !">
-							${row.keterangan}
-						</p>
-					</div>
+            <div
+              class="col-span-2 flex flex-col items-start rounded-xl border border-gray-200 bg-gray-50 p-3">
+              <p class="text-sm text-gray-700">Customer</p>
+              <p class="text-gray-800 text-base font-medium ">
+                ${row.customer_name}
+              </p>
+              <a class="text-gray-800 inline-flex text-base font-medium underline "
+                href="https://api.whatsapp.com/send?phone=${row.customer_telp}&text=Halo, %2A${row.title}%2A. %0A%0ASaya %2A${userName}%2A, marketing dari %2APT. Indodacin Presisi Utama%2A. Saya ingin menghubungi Anda terkait pesanan atau layanan yang mungkin Anda butuhkan.%0A%0AJika ada pertanyaan atau ingin berdiskusi lebih lanjut, silakan balas pesan ini.%0A%0ATerima kasih!%F0%9F%98%8A"
+                target="_blank">
+                Chat customer
+              </a>
+            </div>
 
-					<div
-						class="col-span-2 flex flex-col items-start justify-center rounded-xl border border-gray-200 bg-gray-50 p-3   lg:col-span-1">
-						<p class="text-sm text-gray-700">Status</p>
-						<p class="text-gray-800 pt-1.5 text-base font-medium " id="status">
-              <span class="rounded-xl bg-yellow-100 px-4 py-2 text-sm font-medium text-yellow-800 ring-1 ring-gray-300">
-								Sedang diajukan.
-							</span>
-            </p>
-					</div>
-				</div>
-			</div>`,
+            <div
+              class="col-span-2 flex flex-col items-start rounded-xl border border-gray-200 bg-gray-50 p-3">
+              <p class="text-sm text-gray-700">Lokasi checkpoint</p>
+              <span class="text-gray-800 text-base font-medium ">${row.lokasi}</span>
+              <span class="text-xs font-medium text-gray-400 text-left">
+                <a class="inline-flex underline"
+                  href="https://www.google.com/maps/@${row.latitude},${row.longitude},20m/" target="_blank">
+                  ${row.latitude}, ${row.longitude}
+                </a>
+              </span>
+            </div>
+
+            <div
+              class="col-span-2 flex flex-col items-start rounded-xl border border-gray-200 bg-gray-50 p-3">
+              <p class="mb-2 text-sm text-gray-700">Dokumentasi</p>
+              <div class="relative mx-auto flex-none items-center gap-4 rounded-xl p-2">
+                <img class="h-52 w-52 rounded-xl object-cover transition duration-300 ease-in-out hover:scale-[2]" id="documentations" data-url="${row.photo_collect_relasi[0].photourl}" src="${row.photo_collect_relasi[0].photourl}" alt="" onclick="javascript:void(0)" loading="lazy">
+              </div>
+            </div>
+
+            <div
+              class="col-span-2 flex flex-col items-start rounded-xl border border-gray-200 bg-gray-50 p-3 lg:col-span-1">
+              <p class="text-sm text-gray-700">Keterangan</p>
+              <p class="text-gray-800 text-base font-medium !">
+                ${row.keterangan}
+              </p>
+            </div>
+
+            <div
+              class="col-span-2 flex flex-col items-start rounded-xl border border-gray-200 bg-gray-50 p-3 lg:col-span-1">
+              <p class="text-sm text-gray-700">Status</p>
+              <p class="text-gray-800 pt-1.5 text-base font-medium " id="status">
+                <span class="rounded-xl bg-yellow-100 px-4 py-2 text-sm font-medium text-yellow-800 ring-1 ring-gray-300">
+                  Sedang diajukan.
+                </span>
+              </p>
+            </div>
+          </div>
+			  </div>`,
       confirmButtonText: 'Konfirmasi',
       showDenyButton: true,
       denyButtonText: "Tolak",
@@ -144,6 +165,6 @@ $(document).ready(function () {
             return Swal.fire("ada kegagalan pada server.", `${error}`, "error");
           });
       }
-    } else { Swal.close() }
+    }
   });
 })
