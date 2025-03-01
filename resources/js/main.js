6a4d21bc09d1bb28a342e83127a68cd1673bdd59
razorext2 <razorext2@gmail.com>
2025-03-01 16:35:51 +0700

    remove control button

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,10 +1,6 @@
 const preloader = document.querySelector("#preloader");
-preloader && window.addEventListener("load", (() => {
-  preloader.remove()
-}));
-const scrollContainer = document.getElementById("scrollContainer"),
-  nextButton = document.getElementById("nextButton"),
-  prevButton = document.getElementById("prevButton");
+const themeToggleDarkBtn = document.getElementById("theme-toggle-dark");
+const themeToggleLightBtn = document.getElementById("theme-toggle-light");
 
 function scrollContent(e) {
   scrollContainer.scrollBy({
@@ -13,21 +9,16 @@ function scrollContent(e) {
   })
 }
 
-function updateButtonVisibility() {
-  const e = scrollContainer.scrollWidth > scrollContainer.clientWidth;
-  nextButton.style.display = prevButton.style.display = e ? "block" : "none"
-}
-nextButton.addEventListener("click", (() => scrollContent(1))), prevButton.addEventListener("click", (() =>
-  scrollContent(-1))), updateButtonVisibility(), window.addEventListener("resize", updateButtonVisibility);
-const themeToggleDarkBtn = document.getElementById("theme-toggle-dark"),
-  themeToggleLightBtn = document.getElementById("theme-toggle-light");
-
 function toggleTheme(e) {
   document.documentElement.classList.toggle("dark", e), localStorage.setItem("color-theme", e ? "dark" : "light"),
     themeToggleDarkBtn.classList.toggle("text-gray-300", e), themeToggleDarkBtn.classList.toggle("text-gray-200", !
       e), themeToggleLightBtn.classList.toggle("text-gray-700", e), themeToggleLightBtn.classList.toggle(
         "text-red-400", !e)
 }
+
+preloader && document.addEventListener("livewire:navigated", (() => {
+  preloader.remove()
+}));
 
 document.addEventListener('livewire:navigated', function () {
   const isDarkMode = "dark" === localStorage.getItem("color-theme") || !("color-theme" in localStorage) && window
