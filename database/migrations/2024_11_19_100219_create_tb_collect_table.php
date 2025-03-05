a56fd03493d28f5dcfc5df5fce6edef1b8c3e37e
razorext2 <razorext2@gmail.com>
2025-03-05 16:21:23 +0700

    change bigint to bigInteger

--- database/migrations/2024_11_19_100219_create_tb_collect_table.php
+++ database/migrations/2024_11_19_100219_create_tb_collect_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -23,7 +22,7 @@ return new class extends Migration
             $table->text('notes')->nullable();
             $table->integer('have_paid', 1)->nullable();
             $table->string('payment_type', 32)->nullable();
-            $table->bigint('payment_amount')->nullable();
+            $table->bigInteger('payment_amount')->nullable();
             $table->string('validate_by', 32)->nullable();
             $table->timestamps();
             $table->timestamp('deleted_at')->nullable();
