29f72238b45f460952a3b1ce8dcda43aba8a989d
razorext2 <razorext2@gmail.com>
2025-03-07 09:21:22 +0700

    remove unsued codes from dashboard

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -36,32 +36,7 @@ class AdminController extends Controller
                 ->orderBy('jam_masuk', 'desc')
                 ->get();
 
-            // Subquery to get the latest `jam_keluar` for each `kode_pegawai` for today
-            $attendance_all = Pegawai::with(['attendanceRelasi' => function ($query) use ($today) {
-                $query->whereDate('jam_masuk', $today);
-            }, 'latestAttendanceOutRelasi'])
-                ->select('kode_pegawai', 'full_name', 'nick_name', 'no_telp', 'alamat')
-                ->get()
-                ->map(function ($pegawai) {
-                    return [
-                        'kode_pegawai' => $pegawai->kode_pegawai,
-                        'full_name' => $pegawai->full_name,
-                        'nick_name' => $pegawai->nick_name,
-                        'no_telp' => $pegawai->no_telp,
-                        'alamat' => $pegawai->alamat,
-                        'jam_masuk' => $pegawai->attendanceRelasi->first()->jam_masuk ?? null,
-                        'latest_jam_keluar' => $pegawai->latestAttendanceOutRelasi->jam_keluar ?? null,
-                    ];
-                })
-                ->sortBy(function ($pegawai) {
-                    // Sort by jam_masuk ascending (nulls last) and then by latest_jam_keluar descending (nulls last)
-                    return [
-                        $pegawai['latest_jam_keluar'] ? -$pegawai['latest_jam_keluar']->timestamp : PHP_INT_MAX,  // Sort latest_jam_keluar descending (nulls last)
-                        $pegawai['jam_masuk'] ? $pegawai['jam_masuk']->timestamp : PHP_INT_MAX  // Sort jam_masuk ascending (nulls last)
-                    ];
-                });
-
-            return view('dashboard.dashboard', compact('attendance_today', 'attendance_out_today', 'attendance_all', 'formattedDateRange', 'yearNow'));
+            return view('dashboard.dashboard', compact('attendance_today', 'attendance_out_today', 'formattedDateRange', 'yearNow'));
         } else {
             $getDay = Carbon::today()->locale('id')->isoFormat('dddd');
             $today = Carbon::today();
@@ -89,18 +64,23 @@ class AdminController extends Controller
             $sales_approved_percentage_monthly = $sales_total_monthly > 0 ? $sales_approved_monthly / $sales_total_monthly * 100 : 0;
 
             // Get Pegawai with related Jadwal for today’s day of the week
-            $getPegawai = Pegawai::with(['jadwalRelasi' => function ($query) use ($getDay) {
-                $query->where('hari', $getDay);
-            }])
+            $getPegawai = Pegawai::with([
+                'jadwalRelasi' => function ($query) use ($getDay) {
+                    $query->where('hari', $getDay);
+                }
+            ])
                 ->where('kode_pegawai', Auth::user()->kode_pegawai)
                 ->first();
 
             // Get today's Jadwal if available
             $getJadwal = $getPegawai?->jadwalRelasi->first();
 
-            $attendance_all = Pegawai::with(['attendanceRelasi' => function ($query) use ($today) {
-                $query->whereDate('jam_masuk', $today);
-            }, 'latestAttendanceOutRelasi'])
+            $attendance_all = Pegawai::with([
+                'attendanceRelasi' => function ($query) use ($today) {
+                    $query->whereDate('jam_masuk', $today);
+                },
+                'latestAttendanceOutRelasi'
+            ])
                 ->select('kode_pegawai', 'nick_name')
                 ->where('kode_pegawai', Auth::user()->kode_pegawai)
                 ->get()
@@ -120,7 +100,7 @@ class AdminController extends Controller
                     ];
                 });
 
-            return view('dashboard.dashboard-user', compact(['getDay', 'getJadwal', 'attendance_all', 'sales_total', 'sales_approved', 'sales_approved_percentage',  'sales_total_daily', 'sales_approved_daily', 'sales_approved_percentage_daily', 'sales_total_monthly', 'sales_approved_monthly', 'sales_approved_percentage_monthly']));
+            return view('dashboard.dashboard-user', compact(['getDay', 'getJadwal', 'attendance_all', 'sales_total', 'sales_approved', 'sales_approved_percentage', 'sales_total_daily', 'sales_approved_daily', 'sales_approved_percentage_daily', 'sales_total_monthly', 'sales_approved_monthly', 'sales_approved_percentage_monthly']));
         }
     }
 }
