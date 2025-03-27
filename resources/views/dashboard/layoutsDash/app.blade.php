f7d438287046ab744f45638e27b756e5b06527ba
razorext2 <razorext2@gmail.com>
2025-03-27 11:54:59 +0700

    gajadi ubah

--- resources/views/dashboard/layoutsDash/app.blade.php
+++ resources/views/dashboard/layoutsDash/app.blade.php
@@ -35,7 +35,7 @@
 
 		@include('dashboard.layoutsDash.sidebar')
 
-		<div class="mb-20 mt-32 max-w-screen-xl px-4 sm:ml-72 sm:mt-24 md:mb-4 xl:ml-[450px]">
+		<div class="mb-20 mt-32 max-w-screen-xl px-4 sm:ml-72 sm:mt-24 md:mb-4 xl:ml-96">
 
 			{{-- breadcrumb --}}
 			@livewire('utils.breadcrumb')
