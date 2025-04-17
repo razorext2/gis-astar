f878923f17c99a5eb6d9623939c1fc32c492b3d9
razorext2 <razorext2@gmail.com>
2025-04-17 08:26:11 +0700

    restrict sementara fitur redeem

--- resources/views/dashboard/technicianpoints/index.blade.php
+++ resources/views/dashboard/technicianpoints/index.blade.php
@@ -5,13 +5,16 @@
 
 		<header class="mb-4 flex items-center justify-between">
 			<p class="text-lg font-semibold text-gray-900 dark:text-white">History pendapatan poin</p>
-			<x-button.link class="text-sm text-green-500 ring-1 ring-green-500 hover:bg-green-300 dark:bg-green-800 md:text-base"
-				href="{{ route('technicianpoints.redeem', ['step' => 1]) }}" wire:navigate>
-				<x-slot name="icon">
-					<x-icons.plus class="icon h-6 w-6 text-green-500 dark:text-white" />
-				</x-slot>
-				Redeem
-			</x-button.link>
+
+			@can('point-redeem')
+				<x-button.link class="text-sm text-green-500 ring-1 ring-green-500 hover:bg-green-300 dark:bg-green-800 md:text-base"
+					href="{{ route('technicianpoints.redeem', ['step' => 1]) }}" wire:navigate>
+					<x-slot name="icon">
+						<x-icons.plus class="icon h-6 w-6 text-green-500 dark:text-white" />
+					</x-slot>
+					Redeem
+				</x-button.link>
+			@endcan
 		</header>
 
 		@livewire('handler.point.technician.index')
