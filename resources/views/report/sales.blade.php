631c0eef429765e283e96ec12e3ebbb787279573
razorext2 <razorext2@gmail.com>
2025-04-30 13:03:17 +0700

    adjust excel template

--- resources/views/report/sales.blade.php
+++ resources/views/report/sales.blade.php
@@ -5,13 +5,13 @@
 			<th style="font-weight: bold;" colspan="7">{{ $fromDate . ' s/d ' . $toDate }}</th>
 		</tr>
 		<tr style="height: 70px;">
-			<th style="width: 25px; border: 1px solid black; text-align: center; font-weight: bold;">#</th>
+			<th style="width: 55px; border: 1px solid black; text-align: center; font-weight: bold;">#</th>
 			<th style="width: 120px; border: 1px solid black; text-align: center; font-weight: bold;">Nama Sales</th>
 			<th style="width: 100px; border: 1px solid black; text-align: center; font-weight: bold;">Dibuat Tgl</th>
 			<th style="width: 120px; border: 1px solid black; text-align: center; font-weight: bold;">Judul Laporan</th>
-			<th style="width: 120px; border: 1px solid black; text-align: center; font-weight: bold;">Nama Customer</th>
+			<th style="width: 150px; border: 1px solid black; text-align: center; font-weight: bold;">Nama Customer</th>
 			<th style="width: 100px; border: 1px solid black; text-align: center; font-weight: bold;">No. Telp Customer</th>
-			<th style="width: 120px; border: 1px solid black; text-align: center; font-weight: bold;">Lokasi</th>
+			<th style="width: 180px; border: 1px solid black; text-align: center; font-weight: bold;">Lokasi</th>
 			<th style="width: 120px; border: 1px solid black; text-align: center; font-weight: bold;">Ket.</th>
 			<th style="width: 100px; border: 1px solid black; text-align: center; font-weight: bold;">Diupdate Tgl</th>
 		</tr>
@@ -20,13 +20,13 @@
 		@foreach ($data as $i => $item)
 			<tr style="font-weight: bold;">
 				<td style="border: 1px solid black; text-align: center;">{{ $i + 1 }}</td>
-				<td style="border: 1px solid black; text-align: left;">{{ $item->pegawaiRelasi->full_name ?? '-' }}</td>
+				<td style="border: 1px solid black;">{{ $item->pegawaiRelasi->full_name ?? '-' }}</td>
 				<td style="border: 1px solid black; text-align: center;">{{ $item->created_at ?? '-' }}</td>
-				<td style="border: 1px solid black; text-align: left;">{{ $item->title ?? '-' }}</td>
-				<td style="border: 1px solid black; text-align: left;">{{ $item->customer_name ?? '-' }}</td>
-				<td style="border: 1px solid black; text-align: right; font-size: 9px;">{{ $item->customer_telp ?? '-' }}</td>
-				<td style="border: 1px solid black; text-align: left;">{{ $item->lokasi ?? '-' }}</td>
-				<td style="border: 1px solid black; text-align: left;">{{ strip_tags($item->keterangan) ?? '-' }}</td>
+				<td style="border: 1px solid black;">{{ $item->title ?? '-' }}</td>
+				<td style="border: 1px solid black;">{{ $item->customer_name ?? '-' }}</td>
+				<td style="border: 1px solid black; font-size: 9px;">{{ $item->customer_telp ?? '-' }}</td>
+				<td style="border: 1px solid black;">{{ $item->lokasi ?? '-' }}</td>
+				<td style="border: 1px solid black;">{{ strip_tags($item->keterangan) ?? '-' }}</td>
 				<td style="border: 1px solid black; text-align: center;">{{ $item->updated_at ?? '-' }}</td>
 			</tr>
 		@endforeach
