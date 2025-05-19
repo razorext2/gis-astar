d5cb5c8a6b5a4ea42b7217753cc45c7f44d13a9e
razorext2 <razorext2@gmail.com>
2025-05-19 09:01:57 +0700

    update tampilan count

--- resources/views/livewire/notification-bell.blade.php
+++ resources/views/livewire/notification-bell.blade.php
@@ -4,7 +4,8 @@
 	@if ($notification->count() > 0)
 		<div class="absolute -top-0.5 right-4 block" id="notificationDot" aria-live="polite">
 			<span class="absolute mx-auto inline-flex h-full w-full animate-ping rounded-full bg-yellow-400 opacity-75"></span>
-			<span class="absolute h-4 w-4 rounded-full bg-red-500 text-xs text-white">{{ $notification->count() }}</span>
+			<span
+				class="absolute min-h-4 min-w-4 rounded-full bg-red-500 px-0.5 text-xs text-white">{{ $notification->count() >= 99 ? '99+' : $notification->count() }}</span>
 		</div>
 	@endif
 
