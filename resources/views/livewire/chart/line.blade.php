9e7cae4c4fde9c0ba83de8ac973b972bbe938331
razorext2 <razorext2@gmail.com>
2025-05-22 11:01:09 +0700

    add polling

--- resources/views/livewire/chart/line.blade.php
+++ resources/views/livewire/chart/line.blade.php
@@ -1,3 +1,3 @@
-<div class="h-full">
+<div class="h-full" wire:poll.60s>
 	<livewire:livewire-line-chart key="{{ $multiLineChartModel->reactiveKey() }}" :line-chart-model="$multiLineChartModel" />
 </div>
