1a5f5c859b8b24e8e2cdef85b4e4a14da91e12a6
razorext2 <razorext2@gmail.com>
2025-05-27 14:57:27 +0700

    fix today page pagination

--- resources/views/livewire/handler/attendance/today.blade.php
+++ resources/views/livewire/handler/attendance/today.blade.php
@@ -58,47 +58,50 @@
 			<p class="col-span-2 w-full text-center text-gray-800 dark:text-white"> Belum ada data. </p>
 		@endforelse
 
-		{{ $data->links() }}
+		<div class="col-span-2 w-full">
+			{{ $data->links() }}
+		</div>
 
-		<div wire:show="showModal" wire:transition.duration.300ms
-			class="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-70">
-			@if ($showModal)
-				<!-- Modal box -->
-				<div class="mx-2 flex flex-col gap-2 rounded-xl bg-white p-4 shadow-2xl dark:bg-gray-800 sm:mx-0 md:w-1/3 lg:p-6">
-					<h2 class="text-center text-2xl font-semibold text-gray-900 dark:text-white lg:text-3xl">Detail
-					</h2>
-					<div class="flex w-full flex-col gap-2 text-gray-800 dark:text-white">
+	</div>
 
-						<div class="h-72 lg:h-96">
-							<img onerror="this.onerror=null; this.src='{{ asset('assets/img/noImage.webp') }}'"
-								class="h-full w-full rounded-md object-cover"
-								src="{{ asset(sha1('libs') . '/' . $attendance->photoURL . '.png') }}" alt="">
-						</div>
+	<div wire:show="showModal" wire:transition.duration.300ms
+		class="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-70">
+		@if ($showModal)
+			<!-- Modal box -->
+			<div class="mx-2 flex flex-col gap-2 rounded-xl bg-white p-4 shadow-2xl dark:bg-gray-800 sm:mx-0 md:w-1/3 lg:p-6">
+				<h2 class="text-center text-2xl font-semibold text-gray-900 dark:text-white lg:text-3xl">Detail
+				</h2>
+				<div class="flex w-full flex-col gap-2 text-gray-800 dark:text-white">
 
-						<p class="text-lg font-semibold lg:text-2xl">{{ $attendance->pegawaiRelasi->full_name }}</p>
-						<p class="text-sm text-gray-700 dark:text-gray-400">Melakukan <span class="text-green-400">checkin</span> pada
-							pukul
-							<span class="text-green-400">{{ \Carbon\Carbon::parse($attendance->jam_masuk)->format('H:i:s') }}</span> di
-							<span class="text-green-400">{{ $address }}</span>
-						</p>
-
-						<a class="flex flex-row"
-							href="https://www.google.com/maps/search/?api=1&query={{ $attendance->latitude }},{{ $attendance->longitude }}"
-							target="_blank">Lihat lokasi <x-icons.arrow-right class="ml-1 h-5 w-5 -rotate-45" /></a>
-
-						<div class="flex justify-between">
-							<p class="text-xs">Coord: {{ $attendance->latitude }}, {{ $attendance->longitude }} </p>
-							<p class="text-xs">Created at: {{ $attendance->created_at }}</p>
-						</div>
+					<div class="h-72 lg:h-96">
+						<img onerror="this.onerror=null; this.src='{{ asset('assets/img/noImage.webp') }}'"
+							class="h-full w-full rounded-md object-cover"
+							src="{{ asset(sha1('libs') . '/' . $attendance->photoURL . '.png') }}" alt="">
 					</div>
 
-					<div class="mt-4">
-						<x-button.primary class="w-full justify-center" wire:click="set('showModal', false)">
-							Ok
-						</x-button.primary>
+					<p class="text-lg font-semibold lg:text-2xl">{{ $attendance->pegawaiRelasi->full_name }}</p>
+					<p class="text-sm text-gray-700 dark:text-gray-400">Melakukan <span class="text-green-400">checkin</span> pada
+						pukul
+						<span class="text-green-400">{{ \Carbon\Carbon::parse($attendance->jam_masuk)->format('H:i:s') }}</span> di
+						<span class="text-green-400">{{ $address }}</span>
+					</p>
+
+					<a class="flex flex-row"
+						href="https://www.google.com/maps/search/?api=1&query={{ $attendance->latitude }},{{ $attendance->longitude }}"
+						target="_blank">Lihat lokasi <x-icons.arrow-right class="ml-1 h-5 w-5 -rotate-45" /></a>
+
+					<div class="flex justify-between">
+						<p class="text-xs">Coord: {{ $attendance->latitude }}, {{ $attendance->longitude }} </p>
+						<p class="text-xs">Created at: {{ $attendance->created_at }}</p>
 					</div>
 				</div>
-			@endif
-		</div>
+
+				<div class="mt-4">
+					<x-button.primary class="w-full justify-center" wire:click="set('showModal', false)">
+						Ok
+					</x-button.primary>
+				</div>
+			</div>
+		@endif
 	</div>
 </div>
