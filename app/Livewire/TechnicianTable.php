f70dbb7075618e01cda4796ea4a6dbbaa9c9a9de
razorext2 <razorext2@gmail.com>
2025-07-03 08:50:50 +0700

    change if parameter from checking permission technician-update to technician-approve

--- app/Livewire/TechnicianTable.php
+++ app/Livewire/TechnicianTable.php
@@ -203,7 +203,7 @@ final class TechnicianTable extends PowerGridComponent
                 ->route('technician.show', ['technician' => $row->no_vt]),
         ];
 
-        if ($row->status == 2 || $row->status == 4 || auth()->user()->can('technician-update')) {
+        if ($row->status == 2 || $row->status == 4 || auth()->user()->can('technician-approve')) {
             $button[] = Button::make('edit')
                 ->slot('Update')
                 ->id($row->id)
