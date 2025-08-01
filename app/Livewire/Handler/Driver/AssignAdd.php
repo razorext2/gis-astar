1adef23c81b6e5f6b8288579d27869a52867f586
razorext2 <razorext2@gmail.com>
2025-08-01 14:55:40 +0700

    fix assignadd, remove dumpdie

--- app/Livewire/Handler/Driver/AssignAdd.php
+++ app/Livewire/Handler/Driver/AssignAdd.php
@@ -69,10 +69,6 @@ class AssignAdd extends Component
         // validasi input
         $this->validate();
 
-        $assign_date = $this->assign_date;
-
-        dd($assign_date);
-
         // tampilkan pesan loading
         $this->dispatch('loadingProgress', message: 'Mengirim data...');
 
