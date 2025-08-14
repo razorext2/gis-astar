240d1fbd1981fae00e17279e8e1e9237e3f23fcf
razorext2 <razorext2@gmail.com>
2025-08-14 16:23:14 +0700

    onclick showmodal false

--- resources/views/livewire/handler/attendance/today.blade.php
+++ resources/views/livewire/handler/attendance/today.blade.php
@@ -83,7 +83,7 @@
 
 	</div>
 
-	<div wire:show="showModal" wire:transition.duration.300ms
+	<div wire:show="showModal" wire:click="set('showModal', false)" wire:transition.duration.300ms
 		class="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-70">
 		@if ($showModal)
 			<!-- Modal box -->
