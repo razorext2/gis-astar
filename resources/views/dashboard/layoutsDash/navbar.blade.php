2834139d9a868268b9a754f731593b75228eefff
razorext2 <razorext2@gmail.com>
2025-09-04 10:06:25 +0700

    update -translate-x-20 to -translate-x-40

--- resources/views/dashboard/layoutsDash/navbar.blade.php
+++ resources/views/dashboard/layoutsDash/navbar.blade.php
@@ -2,8 +2,8 @@
 	class="fixed top-0 z-40 w-full border-b border-gray-200 bg-white px-4 py-2.5 shadow-sm dark:border-[#232327] dark:bg-dark-primary dark:shadow-none lg:px-6">
 	<div class="flex flex-wrap items-center justify-between gap-2 sm:flex-nowrap">
 		{{-- title dan icon --}}
-		<div :class="openSidebar ? 'translate-x-0' : 'translate-x-20'"
-			class="flex items-center justify-start transition-all duration-300 ease-out">
+		<div :class="openSidebar ? '-translate-x-40' : 'translate-x-20'"
+			class="flex items-center justify-start transition-all duration-500 ease-out">
 
 			<a class="flex items-center" href="{{ config('app.url') }}">
 				<img class="h-8" src="{{ asset('assets/img/logo.png') }}" alt="Indodacin Logo" loading="lazy" />
