b3a03747216c4e42fdcd1443665bec557423bdc9
razorext2 <razorext2@gmail.com>
2025-11-04 16:34:49 +0700

    ketinggalan, ubah maxfile 4mb coy

--- app/Livewire/Forms/Invoice/Add.php
+++ app/Livewire/Forms/Invoice/Add.php
@@ -43,6 +43,6 @@ class Add extends Form
     #[Validate(['max:30'])]
     public ?int $delivery_status = null;
 
-    #[Validate(['documentations.*' => 'image|max:1024'])]
+    #[Validate(['documentations.*' => 'image|max:4096'])]
     public $documentations = [];
 }
