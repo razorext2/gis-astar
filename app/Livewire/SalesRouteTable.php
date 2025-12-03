5b54f1bc9ab7f019935e0ce5fb8bfde36d833c5c
razorext2 <razorext2@gmail.com>
2025-12-03 10:23:58 +0700

    fix dimana marketing medan melihat semua user tidak hanya sales.

--- app/Livewire/SalesRouteTable.php
+++ app/Livewire/SalesRouteTable.php
@@ -2,21 +2,22 @@
 
 namespace App\Livewire;
 
-use \App\Models\User;
-use Illuminate\Support\Carbon;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Support\Facades\Auth;
 use PowerComponents\LivewirePowerGrid\Button;
 use PowerComponents\LivewirePowerGrid\Column;
 use PowerComponents\LivewirePowerGrid\Facades\Filter;
 use PowerComponents\LivewirePowerGrid\Facades\PowerGrid;
-use PowerComponents\LivewirePowerGrid\PowerGridFields;
 use PowerComponents\LivewirePowerGrid\PowerGridComponent;
+use PowerComponents\LivewirePowerGrid\PowerGridFields;
 
 final class SalesRouteTable extends PowerGridComponent
 {
     public string $tableName = 'SalesRouteTable';
+
     public bool $deferLoading = true;
+
     public $user;
 
     public function setUp(): array
@@ -28,7 +29,7 @@ final class SalesRouteTable extends PowerGridComponent
             PowerGrid::footer()
                 ->showPerPage()
                 ->showRecordCount(),
-            PowerGrid::responsive()
+            PowerGrid::responsive(),
         ];
     }
 
@@ -36,6 +37,10 @@ final class SalesRouteTable extends PowerGridComponent
     {
         $this->user = Auth::user();
         $query = User::query()->with(['pegawai', 'roles']);
+
+        if ($this->user->hasRole(['Marketing'])) {
+            $query->role(['sales']);
+        }
 
         if ($this->user->hasRole(['Marketing-PKU', 'Management-PKU'])) {
             $query->role(['sales-pku']);
@@ -57,7 +62,7 @@ final class SalesRouteTable extends PowerGridComponent
         return [
             'roles' => [
                 'role',
-            ]
+            ],
         ];
     }
 
@@ -68,7 +73,7 @@ final class SalesRouteTable extends PowerGridComponent
             ->add('kode_pegawai')
             ->add('name')
             ->add('email')
-            ->add('role', fn($query) => collect($query->roles->pluck('name'))->implode(', '));
+            ->add('role', fn ($query) => collect($query->roles->pluck('name'))->implode(', '));
     }
 
     public function columns(): array
@@ -107,7 +112,6 @@ final class SalesRouteTable extends PowerGridComponent
         ];
     }
 
-
     public function actions(User $row): array
     {
         return [
@@ -115,7 +119,7 @@ final class SalesRouteTable extends PowerGridComponent
                 ->slot('Detail')
                 ->id($row->id)
                 ->class('rounded-lg px-2.5 py-2 ring-1 ring-green-700 transition-transform duration-300 ease-in-out will-change-transform hover:scale-105 hover:bg-green-300 focus:scale-105 dark:bg-green-800 dark:text-white dark:ring-gray-700 dark:hover:bg-green-900')
-                ->route('routes.sales.detail', ['pegawai' => $row->kode_pegawai ?? 'N/A'])
+                ->route('routes.sales.detail', ['pegawai' => $row->kode_pegawai ?? 'N/A']),
         ];
     }
 }
