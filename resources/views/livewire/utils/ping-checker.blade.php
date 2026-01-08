a99eea01c8c69ced30f6eb008f977f2977c0cea7
razorext2 <razorext2@gmail.com>
2026-01-08 16:41:27 +0700

    update mecanism of ping checker. click to update

--- resources/views/livewire/utils/ping-checker.blade.php
+++ resources/views/livewire/utils/ping-checker.blade.php
@@ -1,40 +1,42 @@
-<div x-data="pingChecker()" x-init="startPolling()">
-	<div class="space-y-1">
-		<p class="{{ $pingClass }} text-sm">
-			{{ $latency == 0 ? 'Loading...' : $latency . ' ms' }}
-		</p>
-	</div>
+<div class="cursor-pointer space-y-1" x-data="pingChecker()" x-init="startPolling()" x-on:click="startPolling()">
+    <p data-popover-target="popover-default" type="button" class="{{ $pingClass }} text-sm">
+        {{ $latency == 0 ? 'Loading...' : $latency . ' ms' }}
+    </p>
+
+    <div data-popover id="popover-default" role="tooltip"
+        class="shadow-xs invisible absolute z-10 inline-block w-64 rounded-lg bg-white text-sm text-gray-800 opacity-0 ring-1 ring-gray-200 transition-opacity duration-300 ease-in-out dark:bg-dark-primary dark:text-white dark:shadow-none dark:ring-gray-700">
+        <div class="px-3 py-2">
+            <p>Silahkan klik untuk memperbarui <span class="font-semibold">ping</span> anda.</p>
+        </div>
+    </div>
 </div>
 
 <script>
-	function pingChecker() {
-		return {
-			startPolling() {
-				// Jalankan pertama kali saat halaman load
-				polling();
+    function pingChecker() {
+        return {
+            startPolling() {
+                // Jalankan pertama kali saat halaman load
+                polling();
+            }
+        }
+    }
 
-				// Jalankan polling berulang setiap 30 detik
-				setInterval(polling, 30000);
-			}
-		}
-	}
+    async function polling() {
+        const start = performance.now();
 
-	async function polling() {
-		const start = performance.now();
+        try {
+            await fetch('{{ route('ping.checker') }}');
+            const end = performance.now();
+            const latency = Math.round(end - start);
 
-		try {
-			await fetch('{{ route('ping.checker') }}');
-			const end = performance.now();
-			const latency = Math.round(end - start);
-
-			Livewire.dispatch('updateLatency', {
-				ms: latency
-			});
-		} catch (error) {
-			console.log(error);
-			Livewire.dispatch('updateLatency', {
-				ms: 9999
-			});
-		}
-	}
+            Livewire.dispatch('updateLatency', {
+                ms: latency
+            });
+        } catch (error) {
+            console.log(error);
+            Livewire.dispatch('updateLatency', {
+                ms: 9999
+            });
+        }
+    }
 </script>
