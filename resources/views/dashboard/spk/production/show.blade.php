6c4a7f4fdda2cf8d75889b7bd77f5fb72b27df99
razorext2 <razorext2@gmail.com>
2026-01-27 15:46:27 +0700

    fix: Correct percentage formatting in production progress display

--- resources/views/dashboard/spk/production/show.blade.php
+++ resources/views/dashboard/spk/production/show.blade.php
@@ -150,7 +150,7 @@
         {{-- progress produksi --}}
         <div class="relative w-full rounded-lg bg-gray-200 dark:bg-gray-700">
             <div class="rounded-lg bg-blue-600 p-4 text-center text-xs font-medium leading-none text-blue-100"
-                style="width: {{ $data->productionHistories?->last()->status_produksi_description['percentage'] ?? '0' . '%' }}">
+                style="width: {{ $data->productionHistories?->last()->status_produksi_description['percentage'] ?? '0' }}%;">
                 {{ $data->productionHistories?->last()->status_produksi_description['label'] ?? '' }}
             </div>
         </div>
