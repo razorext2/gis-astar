9b10042921fa4037ae48fc33197bd1451f1e7366
razorext2 <razorext2@gmail.com>
2026-02-10 13:25:46 +0700

    update lagi

--- resources/views/livewire/handler/production-histories/packing-list.blade.php
+++ resources/views/livewire/handler/production-histories/packing-list.blade.php
@@ -294,7 +294,7 @@
 
                                         <p class="w-full text-center text-xs text-gray-500 dark:text-gray-400">
                                             *Dokumentasi dapat berupa file PNG, JPG, PDF, DOC, XLS (Min, 10KB, Maks
-                                            5MB)
+                                            2MB)
                                         </p>
                                     </div>
                                     <input id="attachment" name="attachment" type="file"
