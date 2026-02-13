7643f512a08175a8fe14cadbaa2bbc32a60e6e20
razorext2 <razorext2@gmail.com>
2026-02-13 15:05:23 +0700

    orderby created_at desc

--- app/Livewire/Handler/Spk/DeliveryBarangList.php
+++ app/Livewire/Handler/Spk/DeliveryBarangList.php
@@ -21,6 +21,7 @@ class DeliveryBarangList extends Component
     {
         $deliveries = SpkDelivery::with('spk.production')
             ->where('id_spk', $this->id)
+            ->orderBy('created_at', 'desc')
             ->paginate(perPage: 4, pageName: 'deliveries');
 
         return view('livewire.handler.spk.delivery-barang-list', [
