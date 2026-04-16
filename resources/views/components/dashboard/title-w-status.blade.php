38fbbd1bbd64dbe97722f4e2f00d52de5ef28a9e
razorext2 <razorext2@gmail.com>
2026-04-16 15:43:58 +0700

    perbaiki title-w-status

--- resources/views/components/dashboard/title-w-status.blade.php
+++ resources/views/components/dashboard/title-w-status.blade.php
@@ -1,27 +1,30 @@
-<div class="flex w-72 flex-row md:w-full">
-	@if ($status == 0)
-		<x-dashboard.status :color="'yellow'">
-			<span class="absolute mx-auto inline-flex h-full w-full animate-ping rounded-md bg-yellow-400 opacity-75"></span>
-			<x-icons.bell-ring class="relative mx-auto inline-flex h-4 w-4" />
-		</x-dashboard.status>
-	@elseif ($status == 1)
-		<x-dashboard.status :color="'green'">
-			<x-icons.check class="mx-auto h-4 w-4" />
-		</x-dashboard.status>
-	@elseif($status == 2 || $status == 4)
-		<x-dashboard.status :color="'yellow'">
-			<span class="absolute mx-auto inline-flex h-full w-full animate-ping rounded-md bg-yellow-400 opacity-75"></span>
-			<x-icons.question-circle class="relative mx-auto inline-flex h-4 w-4" />
-		</x-dashboard.status>
-	@else
-		<x-dashboard.status :color="'red'">
-			<x-icons.close class="mx-auto h-4 w-4" />
-		</x-dashboard.status>
-	@endif
+<div class="flex items-center gap-3 py-1">
+    @if ($status == 0)
+        <x-dashboard.status :color="'yellow'">
+            <span
+                class="absolute inset-0 h-full w-full animate-ping rounded-md bg-yellow-400 opacity-75"></span>
+            <x-icons.bell-ring class="relative mx-auto inline-flex h-4 w-4" />
+        </x-dashboard.status>
+    @elseif ($status == 1)
+        <x-dashboard.status :color="'green'">
+            <x-icons.check class="mx-auto h-4 w-4" />
+        </x-dashboard.status>
+    @elseif($status == 2 || $status == 4)
+        <x-dashboard.status :color="'yellow'">
+            <span
+                class="absolute inset-0 h-full w-full animate-ping rounded-md bg-yellow-400 opacity-75"></span>
+            <x-icons.question-circle class="relative mx-auto inline-flex h-4 w-4" />
+        </x-dashboard.status>
+    @else
+        <x-dashboard.status :color="'red'">
+            <x-icons.close class="mx-auto h-4 w-4" />
+        </x-dashboard.status>
+    @endif
 
-	<div class="max-w-sm text-wrap text-left">
-		<p>{{ $title }}</p>
-		<p class="text-xs text-gray-400">{{ $item3 ?? '' }}</p>
-	</div>
-
+    <div class="flex flex-col text-left">
+        <span class="text-sm font-semibold text-gray-800 dark:text-gray-200">{{ $title }}</span>
+        @if (isset($item3) && $item3)
+            <span class="text-xs text-gray-500 dark:text-gray-400">{{ $item3 }}</span>
+        @endif
+    </div>
 </div>
