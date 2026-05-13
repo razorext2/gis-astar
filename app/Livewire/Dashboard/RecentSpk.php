13f5c98269fc196a10cfd04ac26803527b9087cd
razorext2 <razorext2@gmail.com>
2026-05-13 16:04:05 +0700

    menambah widget quick action

---
+++ app/Livewire/Dashboard/RecentSpk.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Livewire\Dashboard;
+
+use App\Models\Spk\SpkMain;
+use Livewire\Component;
+
+class RecentSpk extends Component
+{
+    public function render()
+    {
+        $recentSpks = collect();
+        if (auth()->user()->can('spk-list')) {
+            $recentSpks = SpkMain::with(['addedBy'])->latest()->take(5)->get();
+        }
+
+        return view('livewire.dashboard.recent-spk', compact('recentSpks'));
+    }
+}
