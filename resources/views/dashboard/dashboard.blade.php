6818a19ecc8340d39bd46a47bd1411dfee41d0d8
razorext2 <razorext2@gmail.com>
2026-05-13 16:05:01 +0700

    menyesuaikan margin bottom antar komponen di quick actions

--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -9,18 +9,25 @@
         </div>
 
         @php
-            $showQuickActions = auth()->user()->can('spk-create') || auth()->user()->can('invoice-list') || auth()->user()->can('spk-validate') || auth()->user()->can('point-redeem');
+            $showQuickActions =
+                auth()->user()->can('spk-create') ||
+                auth()->user()->can('invoice-list') ||
+                auth()->user()->can('spk-validate') ||
+                auth()->user()->can('point-redeem');
             $showRecentSpk = auth()->user()->can('spk-list');
-            $showRightColumn = auth()->user()->can('invoice-list') || auth()->user()->can('technician-list') || auth()->user()->can('point-approve');
+            $showRightColumn =
+                auth()->user()->can('invoice-list') ||
+                auth()->user()->can('technician-list') ||
+                auth()->user()->can('point-approve');
         @endphp
 
-        @if($showQuickActions)
-        <div class="mb-6">
-            <livewire:dashboard.quick-actions />
-        </div>
+        @if ($showQuickActions)
+            <div class="mb-4">
+                <livewire:dashboard.quick-actions />
+            </div>
         @endif
 
-        <div class="mb-6 flex flex-col items-stretch lg:grid lg:grid-cols-3 lg:gap-x-4">
+        <div class="mb-4 flex flex-col items-stretch lg:grid lg:grid-cols-3 lg:gap-x-4">
 
             <form id="attend-in" action="{{ route('attendanceIn.index') }}"></form>
             <form id="attend-out" action="{{ route('attendanceOut.index') }}"></form>
@@ -77,32 +84,33 @@
         </div>
 
         {{-- Command Center Widgets --}}
-        @if($showRecentSpk || $showRightColumn)
-        <div class="grid grid-cols-1 gap-4 lg:grid-cols-3">
-            {{-- SPK Terkini takes 2 columns --}}
-            @if($showRecentSpk)
-            <div class="{{ $showRightColumn ? 'lg:col-span-2' : 'lg:col-span-3' }}">
-                <livewire:dashboard.recent-spk />
-            </div>
-            @endif
-            
-            {{-- Right column for Financial and Leaderboard --}}
-            @if($showRightColumn)
-            <div class="flex flex-col gap-4 {{ $showRecentSpk ? '' : 'lg:col-span-3 lg:flex-row lg:items-start lg:[&>*]:flex-1' }}">
-                @can('invoice-list')
-                <div class="flex-1">
-                    <livewire:dashboard.financial-glance />
-                </div>
-                @endcan
-                
-                @if(auth()->user()->can('technician-list') || auth()->user()->can('point-approve'))
-                <div class="flex-1">
-                    <livewire:dashboard.technician-leaderboard />
-                </div>
+        @if ($showRecentSpk || $showRightColumn)
+            <div class="grid grid-cols-1 gap-4 lg:grid-cols-3">
+                {{-- SPK Terkini takes 2 columns --}}
+                @if ($showRecentSpk)
+                    <div class="{{ $showRightColumn ? 'lg:col-span-2' : 'lg:col-span-3' }}">
+                        <livewire:dashboard.recent-spk />
+                    </div>
+                @endif
+
+                {{-- Right column for Financial and Leaderboard --}}
+                @if ($showRightColumn)
+                    <div
+                        class="{{ $showRecentSpk ? '' : 'lg:col-span-3 lg:flex-row lg:items-start lg:[&>*]:flex-1' }} flex flex-col gap-4">
+                        @can('invoice-list')
+                            <div class="flex-1">
+                                <livewire:dashboard.financial-glance />
+                            </div>
+                        @endcan
+
+                        @if (auth()->user()->can('technician-list') || auth()->user()->can('point-approve'))
+                            <div class="flex-1">
+                                <livewire:dashboard.technician-leaderboard />
+                            </div>
+                        @endif
+                    </div>
                 @endif
             </div>
-            @endif
-        </div>
         @endif
     </div>
 @endsection
