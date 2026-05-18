5672c677f67570148c704799bd4353880776b0a7
razorext2 <razorext2@gmail.com>
2026-05-18 10:56:29 +0700

    perbaiki padding layout index laporan harian

--- resources/views/dashboard/spk/daily-report/general/index.blade.php
+++ resources/views/dashboard/spk/daily-report/general/index.blade.php
@@ -2,21 +2,21 @@
 @section('content')
     @livewire('components.card', ['type' => 'dailyreport'])
 
-    <div
-        class="grid grid-cols-1 gap-2 rounded-xl border border-zinc-200 bg-white/60 p-2 shadow-md backdrop-blur-md dark:border-zinc-800 dark:bg-dark-primary/60 dark:shadow-none lg:gap-4 lg:p-6">
-
-        <div class="flex flex-col md:mb-2">
+    <div class="relative space-y-4">
+        <div
+            class="flex flex-col rounded-xl border border-zinc-200 bg-white/60 p-4 shadow-md backdrop-blur-md dark:border-zinc-800 dark:bg-dark-primary/60 dark:shadow-none lg:p-6">
             <span class="text-xl font-semibold text-gray-900 dark:text-white">
                 Laporan Harian
             </span>
 
-            <p class="mt-0.5 hidden text-gray-600 dark:text-gray-400 lg:block lg:text-base">
+            <p class="text-sm text-gray-600 dark:text-gray-400">
                 Laporan Harian adalah feature yang diperuntukkan untuk staff lapangan seperti Teknisi dan Mekanik
                 untuk pelaporan pekerjaan dilapangan.
             </p>
         </div>
 
-        <div class="flex flex-col gap-2 lg:gap-4">
+        <div
+            class="rounded-xl border border-zinc-200 bg-white/60 px-2 py-4 shadow-md backdrop-blur-md dark:border-zinc-800 dark:bg-dark-primary/60 dark:shadow-none lg:p-6">
             @livewire('project-assignment-table')
         </div>
     </div>
