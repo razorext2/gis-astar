74e2722141da0883cb9a9ec4a91733d6d8bfad46
razorext2 <razorext2@gmail.com>
2026-06-10 16:03:18 +0700

    menggunakan horizon sebagai pengganti queue:worker

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,6 +2,7 @@
 
 return [
     App\Providers\AppServiceProvider::class,
+    App\Providers\HorizonServiceProvider::class,
     App\Providers\RateLimiterServiceProvider::class,
     Yajra\DataTables\DataTablesServiceProvider::class,
 ];
