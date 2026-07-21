1f579499d2597345394899d581b4780370ea5a94
razorext2 <razorext2@gmail.com>
2026-07-21 10:29:00 +0700

    chore: remove unused components for legacy features

--- resources/views/components/detail/label.blade.php
+++
@@ -1,9 +0,0 @@
-@props(['label', 'id'])
-
-<div
-    {{ $attributes->merge(['class' => 'col-span-2 flex flex-col items-start rounded-lg border border-zinc-200 bg-white p-3 dark:border-zinc-800 dark:bg-zinc-900 lg:col-span-1']) }}>
-    <p class="text-sm text-zinc-500 dark:text-zinc-400">{{ $label }}</p>
-    <p class="text-navy-700 text-base font-medium dark:text-white" id="{{ $id }}">
-        {{ $slot }}
-    </p>
-</div>
--- resources/views/components/table-component/confirm-button.blade.php
+++
@@ -1,2 +0,0 @@
-<x-button.primary id="verifikasiBtn-{{ $data->id }}" wire:key="verifikasi-btn-{{ $data->id }}"
-    wire:click="$dispatch('verifikasi', {id: {{ $data->id }}})">Verifikasi</x-button.primary>
